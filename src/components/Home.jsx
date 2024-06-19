566b3a3a2ca5546fedfa135821463d682818f816
Giulia Parca <124640350+GiuParca@users.noreply.github.com>
2024-06-19 23:00:59 +0100

    creating context

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,16 +1,21 @@
+import { useContext } from "react";
+import { QuizContext } from "../context/quiz";
+
 import Quiz from "../images/quiz.png";
 import './Home.css';
 
 const Home = () => {
-  return (
-    <div id="welcome">
-    <h2>Welcome to Trivia&Co.</h2>
-    <img src={Quiz} alt="quiz start image" />
-    <p>Ready to start?</p>
-    <button>Let's go!🤘</button>
-    
-    </div>
-  )
+    const quizState = useContext(QuizContext)
+
+    return (
+        <div id="welcome">
+            <h2>Welcome to Trivia&Co.</h2>
+            <img src={Quiz} alt="quiz start image" />
+            <p>Ready to start?</p>
+            <button>Let's go!🤘</button>
+
+        </div>
+    )
 }
 
 export default Home
